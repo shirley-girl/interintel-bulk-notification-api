87f0273753ed3886ca3695e40b2b11f4185a338a
shirley_girl <mengesashirley@gmail.com>
2026-07-20 21:00:43 +0300

    Update Configuration Environment

--- Core/Core/settings.py
+++ Core/Core/settings.py
@@ -30,10 +30,10 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = [os.environ.get(
+ALLOWED_HOSTS = os.environ.get(
     "ALLOWED_HOSTS",
     "127.0.0.1,localhost"
-).split(",")]
+).split(",")
 
 
 # Application definition
