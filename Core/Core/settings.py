62871af927efce585b78ef85509df261b4d0d3aa
shirley_girl <mengesashirley@gmail.com>
2026-07-20 20:45:39 +0300

    Configure environment

--- Core/Core/settings.py
+++ Core/Core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_u$l#+#o91&%)k3e$5p4+)2%^dv0$iflzyq=bo*uiia6z!+=2y'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-_u$l#+#o91&%)k3e$5p4+)2%^dv0$iflzyq=bo*uiia6z!+=2y"
+)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get(
+    "ALLOWED_HOSTS",
+    "127.0.0.1,localhost"
+).split(",")]
 
 
 # Application definition
